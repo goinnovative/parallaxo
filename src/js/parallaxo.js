a1b175234afc63198a1fadb4a3a0b613359df6e4
Lukas Reusch <lukas.reusch@elairo.de>
2021-09-24 20:15:27 +0200

    Remove transition

--- src/js/parallaxo.js
+++ src/js/parallaxo.js
@@ -38,9 +38,6 @@ const initializeParallax = () => {
     
     // Run parallax to move it to init position
     runParallax( parallaxObject )
-
-    // Mark as initialized with delay to avoid init animation
-    setTimeout(() => element.classList.add('initialized'), 100)
     
     return parallaxObject
   })
